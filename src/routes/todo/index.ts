0dcdcc1865ca57e561aeb1c47738d6c13e95a6db
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-07-15 13:28:41 +0200

    lint and prettier

--- src/routes/todo/index.ts
+++ src/routes/todo/index.ts
@@ -1,24 +1,7 @@
 import { Router } from "express";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
 import TodoModel from "../../database/models/TodoModel";
-import { where } from "sequelize";
-
-let todos = [
-  {
-    id: 1,
-    userId: 1,
-    task: "buy grocery",
-    doBefore: new Date("2024-02-16"),
-    completed: true,
-  },
-  {
-    id: 2,
-    userId: 1,
-    task: "return books",
-    doBefore: new Date("2024-02-20"),
-    completed: false,
-  },
-];
+import { RequestBody } from "../../interfaces/db-models/TodoAttributes";
 
 const TodoRouter = Router();
 
@@ -39,9 +22,7 @@ TodoRouter.get("/todoid", async (req, res) => {
     res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
     return;
   }
-  //const todo = todos.find((item) => item.id == todoId)
-  //const todo = await TodoModel.findByPk(todoId);
-  //({ where: { title: 'My Title' } })
+
   const todo = await TodoModel.findOne({ where: { id: todoId } });
   res.status(StatusCodes.OK).json({ todo });
 });
@@ -51,24 +32,15 @@ TodoRouter.get("/todoid", async (req, res) => {
 // key: userId , value:1
 
 TodoRouter.get("/byuserid", async (req, res) => {
-  const userId =  parseInt(req.query.userId as string);
+  const userId = parseInt(req.query.userId as string);
 
   const todos = await TodoModel.findAll({ where: { userId } });
 
-  res.status(StatusCodes.OK).json( { todos: todos } );
+  res.status(StatusCodes.OK).json({ todos: todos });
 });
 
 //add a single todo
 //http://localhost:5030/v1/todo/addtodo
-// {
-//   "todo": {
-//     "id": 3,
-//     "userId": 2,
-//     "task": ["apple", "banana"],
-//     "doBefore": "2024-03-01",
-//     "completed": false
-//   }
-// }
 
 //JSON body
 // {
@@ -79,7 +51,14 @@ TodoRouter.get("/byuserid", async (req, res) => {
 // }
 
 TodoRouter.post("/addtodo", async (req, res) => {
-  const { newTask, newCompleted, newDoBefore, newUserId } = req.body;
+  const requestBody = req.body as RequestBody;
+
+  const newTask = String(requestBody.newTask);
+  const newCompleted = Boolean(requestBody.newCompleted);
+  const newDoBefore = parseInt(requestBody.newDoBefore, 10);
+  const newUserId = parseInt(requestBody.newUserId, 10);
+
+  //const { newTask, newCompleted, newDoBefore, newUserId } = req.body;
   const newTodo = {
     //id: newId,  increments the id automatically
     task: newTask,
@@ -90,28 +69,28 @@ TodoRouter.post("/addtodo", async (req, res) => {
 
   //todos.push(newTodo);
   const todo = await TodoModel.create(newTodo); // creates a element in the DB
-  // console.log("ID",newTodo.id)
-  // console.log("TAsK",newTodo.task)
-  //res.json({todo: newTodo}),  adds the element to arraywhen there is no DB
   res.status(StatusCodes.OK).json({ todo });
 });
 
 TodoRouter.put("/updatetodo", async (req, res) => {
-  const { todoId, newTask, newDoBefore, newCompleted } = req.body;
+  //const { todoId, newTask, newDoBefore, newCompleted } = req.body;
 
-  // console.log(todoId,newTask,newDoBefore, newCompleted)
-  // const currentTodo = todos.find((item) => item.id == todoId)
+  const requestBody = req.body as RequestBody;
 
-  //   currentTodo.task = newTask;
-  //   currentTodo.completed = newCompleted;
-  //   currentTodo.doBefore = new Date(newDoBefore);
+  const todoId = requestBody.todoId
+    ? parseInt(requestBody.todoId, 10)
+    : undefined;
+  const newTask = String(requestBody.newTask);
+  const newDoBefore = parseInt(requestBody.newDoBefore, 10);
+  const newCompleted = Boolean(requestBody.newCompleted);
+
   await TodoModel.update(
     {
       task: newTask,
       completed: newCompleted,
-      doBefore: newDoBefore,
+      doBefore: new Date(newDoBefore),
     },
-    { where: { id: todoId } }
+    { where: { id: todoId } },
   );
 
   const todo = await TodoModel.findByPk(todoId);
@@ -120,24 +99,21 @@ TodoRouter.put("/updatetodo", async (req, res) => {
 });
 
 TodoRouter.put("/marktodo", async (req, res) => {
-  const { id, newCompleted } = req.body;
-  // console.log("Marked task",id, newCompleted)
+  const requestBody = req.body as RequestBody;
 
-  // const currentTodo = todos.find((item) => item.id == id)
-  // currentTodo.completed = newCompleted
+  const newCompleted = Boolean(requestBody.newCompleted);
+  const todoId = requestBody.todoId
+    ? parseInt(requestBody.todoId, 10)
+    : undefined;
 
-  // const newTodos =  todos.filter((item) => item.id != id)
-  // newTodos.push(currentTodo);
-
-  // todos = newTodos
-  const currentTodo = await TodoModel.update(
+  await TodoModel.update(
     {
       completed: newCompleted,
     },
-    { where: { id: id } }
+    { where: { id: todoId } },
   );
 
-  const updatedTodo = await TodoModel.findByPk(id);
+  const updatedTodo = await TodoModel.findByPk(todoId);
 
   res.status(StatusCodes.OK).json({ updatedTodo: updatedTodo });
 });
@@ -147,10 +123,10 @@ TodoRouter.put("/marktodo", async (req, res) => {
 // }
 
 TodoRouter.delete("/deletetodo", async (req, res) => {
-  const { todoId } = req.body;
-
-  // const currentTodos =  todos.filter((item) => item.id !== toId);
-  // console.log(`Deleted todo id: ${toId}`)
+  const requestBody = req.body as RequestBody;
+  const todoId = requestBody.todoId
+    ? parseInt(requestBody.todoId, 10)
+    : undefined;
 
   const deleted = await TodoModel.destroy({ where: { id: todoId } });
   if (deleted) {
