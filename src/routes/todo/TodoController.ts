cd4e7a27e68220e0e5604abccf58b1a590d441c8
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-07-17 18:37:00 +0200

    refer later

--- src/routes/todo/TodoController.ts
+++ src/routes/todo/TodoController.ts
@@ -1,50 +1,124 @@
+// src/routes/todo/todoController.ts
 import {
-    Body,
-    Controller,
-    Get,
-    Post,
-    Route,
-    Request,
-    SuccessResponse,
-    Tags,
-  } from '@tsoa/runtime';
-  import { Request as ExpressRequest } from 'express';
-  import { StatusCodes } from 'http-status-codes';
-  import TodoModel from '../../database/models/TodoModel';
-  import { TodoAttributes, RequestBody } from "../../interfaces/db-models/TodoAttributes"
-  
-  export type TodoResponse = boolean;
-  
-  @Route('v1/todo')
-  @Tags('Todo')
-  class TodoController extends Controller {
-    @Get('/todos')
-    @SuccessResponse(StatusCodes.OK)
-    public async getAllTodos(): Promise<TodoAttributes[]> {
-      const todos = await TodoModel.findAll();
-      return todos as TodoAttributes[];
-    }
-  
-    @Post('/addtodo')
-    @SuccessResponse(StatusCodes.CREATED)
-    public async addTodo(
-      @Request() req: ExpressRequest,
-      @Body() body: RequestBody,
-    ): Promise<TodoResponse | Error> {
-      const { newTask, newCompleted, newDoBefore, newUserId } = body;
-  
-      const newTodo: Omit<TodoAttributes, 'id'> = {
-        task: newTask,
-        completed: newCompleted === "true",
-        doBefore: new Date(newDoBefore),
-        userId: parseInt(newUserId, 10)
-      };
-  
-      await TodoModel.create(newTodo);
-      return true;
-    }
-  
+  Body,
+  Controller,
+  Get,
+  Post,
+  Route,
+  Request,
+  SuccessResponse,
+  Tags,
+  Query,
+  // Put,
+  // Delete
+} from '@tsoa/runtime';
+import { Request as ExpressRequest } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import TodoModel from '../../database/models/TodoModel';
+import { TodoAttributes, RequestBody } from "../../interfaces/db-models/TodoAttributes";
+
+export type TodoResponse = boolean;
+
+
+@Route('v1/todo')
+@Tags('Todo')
+class TodoController extends Controller {
+  @Get('/todos')
+  @SuccessResponse(StatusCodes.OK)
+  public async getAllTodos(): Promise<TodoAttributes[]> {
+    const todos = await TodoModel.findAll();
+    return todos as TodoAttributes[];
   }
+
+  @Post('/addtodo')
+  @SuccessResponse(StatusCodes.CREATED)
+  public async addTodo(
+    @Request() req: ExpressRequest,
+    @Body() body: RequestBody,
+  ): Promise<TodoResponse | Error> {
+    const { newTask, newCompleted, newDoBefore, newUserId } = body;
+
+    const newTodo: Omit<TodoAttributes, 'id'> = {
+      task: newTask,
+      completed: newCompleted === "true",
+      doBefore: new Date(newDoBefore),
+      userId: parseInt(newUserId, 10)
+    };
+
+    await TodoModel.create(newTodo);
+    return true;
+  }
+
+  @Get('/todoid')
+  public async getTodoById(@Query() todoId: string) {
+    if (!todoId) {
+      this.setStatus(StatusCodes.BAD_REQUEST);
+      return;
+    }
+
+    const todo = await TodoModel.findOne({ where: { id: todoId } });
+    return todo;
+  }
+
+  // @Get('/byuserid')
+  // public async getTodosByUserId(@Query() userId: number) {
+  //   const todos = await TodoModel.findAll({ where: { userId } });
+  //   return todos;
+  // }
+
+  // @Put('/updatetodo')
+  // public async updateTodo(
+  //   @Request() req: ExpressRequest,
+  //   @Body() body: RequestBody,
+  // ) {
+  //   const { todoId, newTask, newDoBefore, newCompleted } = body;
+
+  //   await TodoModel.update(
+  //     {
+  //       task: newTask,
+  //       completed: newCompleted === "true",
+  //       doBefore: new Date(newDoBefore),
+  //     },
+  //     { where: { id: todoId } },
+  //   );
+
+  //   const todo = await TodoModel.findByPk(todoId);
+  //   return todo;
+  // }
+
+  // @Put('/marktodo')
+  // public async markTodo(
+  //   @Request() req: ExpressRequest,
+  //   @Body() body: RequestBody,
+  // ) {
+  //   const { newCompleted, todoId } = body;
+
+  //   await TodoModel.update(
+  //     {
+  //       completed: newCompleted === "true"
+  //     },
+  //     { where: { id: todoId } },
+  //   );
+
+  //   const updatedTodo = await TodoModel.findByPk(todoId);
+  //   return updatedTodo;
+  // } 
+
+  // @Delete('/deletetodo')
+  // public async deleteTodo(
+  //   @Request() req: ExpressRequest,
+  //   @Body() body: RequestBody,
+  //   ) {
+  //     const { todoId } = body;
   
-  export default TodoController;
-  
+  //     const deleted = await TodoModel.destroy({ where: { id: todoId } });
+  //     if (deleted) {
+  //       return { deletedTodos: todoId };
+  //     } else {
+  //       this.setStatus(StatusCodes.NOT_FOUND);
+  //       return "Todo not found";
+  //     }
+  //   }
+}
+
+export default TodoController;
--- src/routes/todo/TodoRouter.ts
+++ src/routes/todo/TodoRouter.ts
@@ -1,23 +1,100 @@
 import { Router, Request, Response, NextFunction } from "express";
 import TodoController from "./TodoController";
 import { RequestBody } from "../../interfaces/db-models/TodoAttributes";
-import { ParamsDictionary } from 'express-serve-static-core';
+import { ParamsDictionary } from "express-serve-static-core";
 
 const todoController = new TodoController();
 
 const TodoRouter = Router();
 
-TodoRouter.get("/todos", 
-    (req, res, next) => todoController.getAllTodos()
-    .then(data => res.json(data))
-    .catch(next));
+TodoRouter.get("/todos", (req, res, next) =>
+  todoController
+    .getAllTodos()
+    .then((data) => res.json(data))
+    .then((data) => res.json(data))
+    .catch(next),
+);
 
-TodoRouter.post("/addtodo", (req: Request<ParamsDictionary, RequestBody>, res: Response, next: NextFunction) => {
-    todoController.addTodo(req, req.body as RequestBody)
-        .then(data => res.json(data))
-        .catch(next);
-});
-    
+TodoRouter.post(
+  "/addtodo",
+  (
+    req: Request<ParamsDictionary, RequestBody>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    todoController.addTodo(req, req.body as RequestBody).catch(next);
+  },
+);
+
+TodoRouter.get(
+  "/todoid",
+  (
+    req: Request<ParamsDictionary, unknown, unknown, { todoId: string }>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    todoController
+      .getTodoById(req.query.todoId)
+      .then((data) => res.json(data))
+      .catch(next);
+  },
+);
+
+// TodoRouter.get(
+//   "/byuserid",
+//   (
+//     req: Request<ParamsDictionary, unknown, unknown, { userId: number }>,
+//     res: Response,
+//     next: NextFunction,
+//   ) => {
+//     todoController
+//       .getTodosByUserId(req.query.userId)
+//       .then((data) => res.json(data))
+//       .catch(next);
+//   },
+// );
+
+// TodoRouter.put(
+//   "/updatetodo",
+//   (
+//     req: Request<ParamsDictionary, RequestBody>,
+//     res: Response,
+//     next: NextFunction,
+//   ) => {
+//     todoController
+//       .updateTodo(req, req.body as RequestBody)
+//       .then((data) => res.json(data))
+//       .catch(next);
+//   },
+// );
+
+// TodoRouter.put(
+//   "/marktodo",
+//   (
+//     req: Request<ParamsDictionary, RequestBody>,
+//     res: Response,
+//     next: NextFunction,
+//   ) => {
+//     todoController
+//       .markTodo(req, req.body as RequestBody)
+//       .then((data) => res.json(data))
+//       .catch(next);
+//   },
+// );
+
+// TodoRouter.delete(
+//   "/deletetodo",
+//   (
+//     req: Request<ParamsDictionary, RequestBody>,
+//     res: Response,
+//     next: NextFunction,
+//   ) => {
+//     todoController
+//       .deleteTodo(req, req.body as RequestBody)
+//       .then((data) => res.json(data))
+//       .catch(next);
+//   },
+// );
 
 // TodoRouter.get("/todos", TodoController.getAllTodos);
 // TodoRouter.get("/todoid", TodoController.getTodoById);
